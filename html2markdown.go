191bfa8fd7aaee526a64ee049b8170022bb236f8
jaydenwen123 <2282186474@qq.com>
2019-08-25 21:39:17 +0800

    update markdown

--- html2markdown.go
+++ html2markdown.go
@@ -1,20 +1,21 @@
 package util
 
 import (
+	"github.com/jaydenwen123/html2text"
 	"io/ioutil"
-	"jaytaylor.com/html2text"
 	"os"
 )
+
 //根据html文件生成markdown文件
 //htmlFile:html文件名
 //markdownFile:markdown文件名
-func MarkdownFromHtmlFile(htmlFile string,markdownFile string) error{
-	hFile,err:=os.Open(htmlFile)
+func MarkdownFromHtmlFile(htmlFile string, markdownFile string) error {
+	hFile, err := os.Open(htmlFile)
 	if err != nil {
 		//panic(err)
 		return err
 	}
-	markText, err := html2text.FromReader(hFile,html2text.Options{PrettyTables: true})
+	markText, err := html2text.FromReader(hFile, html2text.Options{PrettyTables: true})
 	if err != nil {
 		return err
 	}
@@ -28,13 +29,13 @@ func MarkdownFromHtmlFile(htmlFile string,markdownFile string) error{
 //根据html字符串生成markdown文件
 //htmlStr：html字符串
 //markdownFile：markdown文件名
-func MarkdownFromHtmlString(htmlStr string,markdownFile string) error {
+func MarkdownFromHtmlString(htmlStr string, markdownFile string) error {
 	markText, err := html2text.FromString(htmlStr, html2text.Options{PrettyTables: true})
 	if err != nil {
 		return err
 	}
-	err= ioutil.WriteFile(markdownFile, []byte(markText), os.ModeType)
-	if err!=nil{
+	err = ioutil.WriteFile(markdownFile, []byte(markText), os.ModeType)
+	if err != nil {
 		return err
 	}
 	return nil
@@ -43,6 +44,6 @@ func MarkdownFromHtmlString(htmlStr string,markdownFile string) error {
 //根据html字节内容生成markdown文件
 //htmlData：html字节内容
 //markdownFile：markdown文件名
-func MarkdownFromHtmlBytes(htmlData []byte,markdownFile string) error {
-	return MarkdownFromHtmlString(string(htmlData),markdownFile)
+func MarkdownFromHtmlBytes(htmlData []byte, markdownFile string) error {
+	return MarkdownFromHtmlString(string(htmlData), markdownFile)
 }

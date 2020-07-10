fca05c80aa0c6a22cccb96d0899e02723a502e36
jaydenwen123 <2282186474@qq.com>
2020-07-10 10:25:03 +0800

    feat(file.go):add WriteData2File api

--- costtime.go
+++ costtime.go
@@ -15,35 +15,35 @@ func NewCost(start time.Time) *Cost {
 	return &Cost{Start: start}
 }
 
-func (this *Cost) SetEndTime(end time.Time)  {
-		this.End=end
+func (c *Cost) SetEndTime(end time.Time)  {
+		c.End=end
 }
 
 
 //设置终止时间
-func (this *Cost) CalcCost() time.Duration {
-		//return this.
-	duration := this.End.Sub(this.Start)
+func (c *Cost) CalcCost() time.Duration {
+		//return c.
+	duration := c.End.Sub(c.Start)
 	//return fmt.Sprintf("%+v",duration)
 	return duration
 }
 //设置终止时间
-func (this *Cost) ShowCalcCostAsString() string {
-		//return this.
-	return fmt.Sprintf("%+v",this.CalcCost())
+func (c *Cost) ShowCalcCostAsString() string {
+		//return c.
+	return fmt.Sprintf("%+v", c.CalcCost())
 }
 
 //不用设置终止时间
-func (this *Cost) CostWithNow() time.Duration {
-	//return this.
-	duration := time.Since(this.Start)
+func (c *Cost) CostWithNow() time.Duration {
+	//return c.
+	duration := time.Since(c.Start)
 	//return fmt.Sprintf("%+v",duration)
 	return duration
 }
 
-func (this *Cost) CostWithNowAsString() string {
-		//return this.
-	return fmt.Sprintf("%+v",this.CostWithNow())
+func (c *Cost) CostWithNowAsString() string {
+		//return c.
+	return fmt.Sprintf("%+v", c.CostWithNow())
 }
 
 
--- file.go
+++ file.go
@@ -1,10 +1,39 @@
 package util
 
-import "regexp"
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/astaxie/beego/logs"
+)
 
 var cmp = regexp.MustCompile(`([\/:*?"<>|]+)`)
-//去除文件名中的非法字符
-func TrimIllegalStr(filename string,replaceStr string) string {
+
+//TrimIllegalStr 去除文件名中的非法字符
+func TrimIllegalStr(filename string, replaceStr string) string {
 	newStr := cmp.ReplaceAllString(filename, replaceStr)
 	return newStr
+}
+
+//WriteData2File 将数据写入到文件
+func WriteData2File(basepath, filename, content string, perm os.FileMode) error {
+	var err error
+
+	if ! IsExist(basepath) {
+		err = InitDir(basepath)
+		if err != nil {
+			logs.Error("Init the dir<%s> conf error:%s", basepath, err.Error())
+			return err
+		}
+	}
+	shellFileName := filepath.Join(basepath, filename)
+	//err = util.AppendLine2File(shellFileName, content)
+	err = ioutil.WriteFile(shellFileName, []byte(content), perm)
+	if err != nil {
+		logs.Error("init the <%s> error:%s", shellFileName, err.Error())
+		return err
+	}
+	return nil
 }
